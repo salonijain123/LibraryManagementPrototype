c47d1024e53e6cf79a612ec11265d75919412044
salonijain123 <salonijain258@gmail.com>
2021-07-09 23:28:19 +0530

    consolidated commit

---
+++ src/main/java/com/javatpoint/Student.java
@@ -0,0 +1,26 @@
+/*
+package com.javatpoint.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="student")
+@NamedQuery(name = "find name" , query = "Select s from StudentEntity s")
+@Data
+public class Student {
+
+    @Id
+    @Column(name="student_id")
+    private int s_id;
+
+    @Column(name="student_firstName")
+    private String firstName;
+
+    @Column(name="student_lastName")
+    private int lastName;
+
+
+}
+*/
