9a869e02fc3311c956d8b7c59b5a05c99a537ce2
salonijain123 <salonijain258@gmail.com>
2021-07-09 22:41:05 +0530

    CommittoBranch1

--- src/main/java/com/javatpoint/Constants.java
+++ src/main/java/com/javatpoint/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
     public static final String QUANTITY_MORE_THAN_ONE ="book can be issued as its quantity is  ::";
     public static final String QUANTITY_MORE_THAN_TWO ="book can be issued as its quantity is  ::";
     public static final String QUANTITY_LESS_THAN_ONE ="book cant be issued as its less than 1 quantity";
+    public static final String QUANTITY_LESS_THAN_ONE ="book cant be issued as its less than 1 quantity";
     public static final String getJoinResult ="SELECT inv_count.bookid as book_id, books.bookname as book_name\n" +
             "        FROM inv_count\n" +
             "        INNER JOIN books ON books.bookid = inv_count.bookid where books.bookname = :bName\n" +
